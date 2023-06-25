245d252d8139061ecefe0f1d322cbdf6630689c6
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-25 11:57:24 +0600

    layout and component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>
+            2023 Pictorial, All rights reserved.
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import React from "react";
+
+const navData = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 3,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 4,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link href="/">Pictorial</Link>
+      <div>
+        {navData.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
