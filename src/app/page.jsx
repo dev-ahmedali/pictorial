8bb5fd44faefac0c0ccc5e286969336233191ba3
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-26 03:07:41 +0600

    updated hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import hero from "../../public/images/hero.png";
 
 export default function Home() {
   return (
-     <div>
-         Home
-     </div>
-  )
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1>Better design for your digital products</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+          sequi atque ad aliquid fugiat quaerat!
+        </p>
+      </div>
+      <button>See Our Work</button>
+      <div className={styles.item}></div>
+      <Image src={hero} alt="hero section" className={styles.heroImg} />
+    </div>
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>2023 Pictorial, All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          className={styles.icon}
+          src="/../public/images/1.png"
+          width={15}
+          height={15}
+          alt="pictorial Facebook"
+        />
+        <Image
+          className={styles.icon}
+          src="/../public/images/2.png"
+          width={15}
+          height={15}
+          alt="pictorial Instagram"
+        />
+        <Image
+          className={styles.icon}
+          src="/../public/images/3.png"
+          width={15}
+          height={15}
+          alt="pictorial Twitter"
+        />
+        <Image
+          className={styles.icon}
+          src="/../public/images/4.png"
+          width={15}
+          height={15}
+          alt="pictorial Youtube"
+        />
+      </div>
     </div>
   );
 };
