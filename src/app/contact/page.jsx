11222a745b8c953d078e4c5ea8fc16b4b672724f
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-28 01:23:41 +0600

    updated about and contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import styles from "./page.module.css"
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const Contact = () => {
   return (
-    <div className={styles.container}>Contact page</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src='/images/contact.png' alt="" fill={true}/>
+        </div>
+        <form className={styles.form}></form>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
