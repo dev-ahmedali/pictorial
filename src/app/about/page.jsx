b2d9f0067046fea655eace8d98ade7e0c03c62b0
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-27 14:33:33 +0600

    updated about ui

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -22,8 +22,8 @@ const About = () => {
       </div>
       <div className={styles.textContainer}>
         <div className={styles.item}>
-          <h1 className="title">Who Are We?</h1>
-          <p className="desc">
+          <h1 className={styles.title}>Who Are We?</h1>
+          <p className={styles.desc}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos porro
             vero ipsum autem tenetur fugit natus quisquam consequatur itaque
             neque delectus nulla numquam omnis incidunt, eius ex sint
@@ -37,8 +37,8 @@ const About = () => {
           </p>
         </div>
         <div className={styles.item}>
-        <h1 className="title">What We Do?</h1>
-        <p className="desc">
+        <h1 className={styles.title}>What We Do?</h1>
+        <p className={styles.desc}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos porro
             vero ipsum autem tenetur fugit natus quisquam consequatur itaque
             neque delectus nulla numquam omnis incidunt, eius ex sint
